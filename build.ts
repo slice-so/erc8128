28387dc95b88fa34ae1616b25e3042a35be369fa
Dom Mac <domenicochad@gmail.com>
2026-02-08 14:15:44 +0100

    chore(erc8128): prepare public sync

--- build.ts
+++ build.ts
@@ -1,3 +1,46 @@
-import { buildPackage } from "../../build"
+/// <reference types="bun-types" />
+import dts, { type Options } from "bun-plugin-dts"
+
+interface BuildConfig {
+  entrypoint?: string
+  external?: string[]
+  dtsConfig?: Options
+  emitTypes?: boolean
+  minify?: boolean
+  sourcemap?: "none" | "inline" | "external"
+  target?: "node" | "browser"
+  outdir?: string
+  watch?: boolean
+}
+
+export async function buildPackage({
+  entrypoint = "./src/index.ts",
+  external = [],
+  dtsConfig,
+  emitTypes = process.env.EMIT_TYPES !== "false",
+  minify = true,
+  sourcemap = "external",
+  target = "browser",
+  outdir = "./dist/esm"
+}: BuildConfig = {}) {
+  const result = await Bun.build({
+    entrypoints: [entrypoint],
+    outdir,
+    external,
+    format: "esm",
+    minify,
+    sourcemap,
+    target,
+    plugins: emitTypes ? [dts(dtsConfig)] : []
+  })
+
+  if (!result.success) {
+    throw new AggregateError(result.logs, "Build failed")
+  }
+
+  console.log("Build successful in ", outdir)
+
+  return result
+}
 
 await buildPackage()
